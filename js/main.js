8d7cb80ab1a268e4f7ed17f3b3508d9220e37f8d
cristinatato <c.tatodelatorre@gmail.com>
2019-07-26 11:27:28 +0200

    add first structure and js actions

---
+++ js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+const app = document.querySelector ('.app');
+const updateBtn = document.querySelector ('.app__update');
+const selectMood = document.querySelector ('.app__mood');
+const face = document.querySelector ('.app__face');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function updateMood () {
+    // Recogerá el nuevo estado del select y lo pintará en lugar del actual
+    face.innerHTML = selectMood.value;
+    // Generará un número aleatorio de 100 como máximo
+    const randomNum = getRandomInt (100);
+    // console.log (randomNum);   
+    //  y si es un número par colocará el color de fondo del amarillo correcto (#ffcc00) y si es impar, usaremos el naranja fuego chileno (#ff9900)
+    if (randomNum%2 === 0){
+        // console.log ('es par');
+        app.classList.remove('app__alt');
+    } else {
+        // console.log ('es impar');
+        app.classList.add('app__alt');
+    }
+}
+
+updateBtn.addEventListener('click', updateMood);
+
+
+
+
+ 
